55e53033d6b6cd22842f5c839e39bcd80c2eac1f
Haofu <haofu.zhu@gmail.com>
2018-02-13 14:40:23 +1100

    add listener to 'enter' being pressed

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@ $(document).ready(() => {
   console.log("ready");
 
   $('.set-btn').on('click', e=>{
-    const color = $('.color-input').val();
-    $('.brush').css('background-color', color)
+    changeColor();
+  })
+
+  $(document).on('keyup', e=>{
+    if (e.keyCode === 13) {
+      changeColor();
+    }
   })
 });
+
+const changeColor = () => {
+  const color = $('.color-input').val();
+  $('.brush').css('background-color', color);
+  $('.color-input').val('');
+}
